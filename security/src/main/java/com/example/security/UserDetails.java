7630f370bb7cb712bcdd9eeadc3f964fbaf8aa65
marco.madalin.farcas <marco.madalin.farcas@est.fib.upc.edu>
2023-10-20 14:25:58 +0200

    setup security service

---
+++ security/src/main/java/com/example/security/UserDetails.java
@@ -0,0 +1,49 @@
+package com.example.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+
+    private String username;
+
+    private String password;
+
+    private String organization;
+
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ security/src/main/java/com/example/security/UserDetailsService.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        return null;
+    }
+}
